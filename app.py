8a70912b9714052e203cf9853eda6479d4dca2fa
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2023-05-21 21:24:13 -0300

    ultimas alterações

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def handle_webhook():
     return 'Webhook recebido'
 
 def liberar_acesso(nome, email):
-    status_atual = f"Acesso liberado {nome} {email},\n Seja muito bem-vindo à nossa plataforma!!)"
+    status_atual = f"Acesso liberarado {nome} {email},\n Seja muito bem-vindo à nossa plataforma!!)"
     print(status_atual)
     return status_atual
 
@@ -144,11 +144,11 @@ def enviar_mensagem_pagamento_recusado(nome, email):
     return status_atual
 
 def remover_acesso(nome, email):
-    status_atual = f"Acesso removido! {nome} ({email})"
+    status_atual = f"Remover acesso do cliente {nome} ({email})"
     print(status_atual)
     return status_atual
 
-@app.route('/filtar_tratativas', methods=['GET'])
+@app.route('/filtrar_tratativas', methods=['GET'])
 def filtrar_tratativas():
     email = request.args.get('email')
     if email:
